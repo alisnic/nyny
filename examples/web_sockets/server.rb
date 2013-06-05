1f890488aa121577cbb5a8aa80705d9e923c18a0
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-06-05 14:05:21 +0300

    Add websocket example

---
+++ examples/web_sockets/server.rb
@@ -0,0 +1,52 @@
+#!ruby -I ../../lib -I lib
+require 'frankie'
+require 'json'
+require 'faye/websocket'
+
+# Note:
+#   Currently, websocket-rack gem only works with Thin
+#
+
+Faye::WebSocket.load_adapter('thin')
+
+class Sockets
+  def initialize app=nil, opts={}
+    @app = app
+    @path = opts.fetch :path, '/'
+  end
+
+  def call env
+    return @app.call(env) unless env['PATH_INFO'] == @path
+
+    if Faye::WebSocket.websocket?(env)
+      ws = Faye::WebSocket.new(env)
+      handle ws
+      ws.rack_response
+    else
+      @app.call(env)
+    end
+  end
+
+  def handle ws
+    ws.on :message do |event|
+      ws.send(event.data)
+    end
+
+    ws.on :close do |event|
+      p [:close, event.code, event.reason]
+      ws = nil
+    end
+  end
+end
+
+class App < Frankie::App
+  #Serve static assets from public folder
+  use Rack::Static, :urls => ["/public"]
+  use Sockets, :path => '/websocket'
+
+  get '/frankie' do
+    'yep, you can still use frankie'
+  end
+end
+
+App.run! 9000
