7eec1025ff28a6904d2e2eb2aa20207177456b34
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-10-06 23:51:20 +0300

    Add session example

---
+++ examples/session.rb
@@ -0,0 +1,20 @@
+#!ruby -I ../lib -I lib
+require 'nyny'
+
+class App < NYNY::App
+  use Rack::Session::Cookie #store session in cookies
+
+  before do
+    headers 'Content-Type' => 'text/plain'
+  end
+
+  get '/' do
+    session.merge! params
+    "Use ? in URL to write to session
+     Eg: /?foo=bar
+
+     Session: #{session.inspect}"
+  end
+end
+
+App.run! 9000
