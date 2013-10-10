c481d739db6065477f9b19fffad6a54a276e21fd
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-10-10 09:30:06 +0300

    Added NYNY.env

--- lib/nyny.rb
+++ lib/nyny.rb
@@ -15,4 +15,20 @@ require 'nyny/core-ext/runner'
 
 module NYNY
   App.register NYNY::Runner
+
+  class EnvString < String
+    [:production, :development, :test].each do |env|
+      define_method "#{env}?" do
+        self == env.to_s
+      end
+    end
+  end
+
+  def self.root
+    Dir.pwd
+  end
+
+  def self.env
+    @env ||= EnvString.new(ENV['RACK_ENV'] || 'development')
+  end
 end
