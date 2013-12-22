80e3dfa808a5929876e1fea0cbb119ec60c9da6e
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-12-22 16:55:08 +0200

    Change the example to use the templates stuff

--- examples/templates/server.rb
+++ examples/templates/server.rb
@@ -1,27 +1,9 @@
 #!ruby -I ../../lib -I lib
 require 'nyny'
-require 'sinatra'
-require 'ostruct'
-
-module Views
-  CACHE = Tilt::Cache.new
-  include ::Sinatra::Templates
-
-  def settings
-    @settings ||= OpenStruct.new :templates => {}
-  end
-
-  def template_cache
-    CACHE
-  end
-end
-
 
 class App < NYNY::App
-  helpers Views
-
   get '/' do
-    haml :index
+    render 'views/index.haml'
   end
 end
 
