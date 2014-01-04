d3aa6564abbcf128801b409e41353e8a6dbc3ff4
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-01-04 21:22:10 +0200

    Optional parent class as argument to mock builders

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,12 +27,12 @@ def extended_modules_for kls
   (class << kls; self end).included_modules
 end
 
-def mock_app &blk
-  Rack::MockRequest.new mock_app_class(&blk).new
+def mock_app parent=App, &blk
+  Rack::MockRequest.new mock_app_class(parent, &blk).new
 end
 
-def mock_app_class &blk
-  Class.new(App, &blk)
+def mock_app_class parent=App, &blk
+  Class.new(parent, &blk)
 end
 
 def random_url levels=1
