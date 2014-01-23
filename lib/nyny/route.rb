d12809aa50986baa4f108e3a765142467f29dbc1
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-01-24 00:44:48 +0200

    Use Journey::Path::Pattern and avoid headaches with regular expressions

--- lib/nyny/route.rb
+++ lib/nyny/route.rb
@@ -1,30 +1,12 @@
+require 'journey'
+
 module NYNY
   class Route
-    NAME_PATTERN = /:(\S+)/
-
     attr_reader :pattern, :handler, :method
     def initialize method, signature, &block
-      @pattern = pattern_for signature
+      @pattern = Journey::Path::Pattern.new(signature)
       @handler = Proc.new(&block)
       @method = method.to_s.upcase
-    end
-
-    def pattern_for signature
-      return signature if signature.is_a? Regexp
-      build_regex(signature.start_with?('/') ? signature : "/#{signature}")
-    end
-
-    def build_regex signature
-      return %r(^#{signature}$) unless signature.include?(':')
-
-      groups = signature.split('/').map do |part|
-        next part if part.empty?
-        next part unless part.start_with? ':'
-        name = NAME_PATTERN.match(part)[1]
-        %Q{(?<#{name}>\\S+)}
-      end.select {|s| !s.empty? }.join('\/')
-
-      %r(^\/#{groups}$)
     end
 
     def match? env
@@ -34,7 +16,7 @@ module NYNY
 
     def url_params env
       data = pattern.match(env['PATH_INFO'])
-      Hash[data.names.map {|n| [n.to_sym, URI.unescape(data[n])]}]
+      Hash[data.names.zip(data.captures)]
     end
   end
 end
