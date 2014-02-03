2209f8f5c4a42907fc288531e2d662ed001392d2
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-02-03 20:58:48 +0200

    Remove sprockets nonsense, since sprockets integration is deloped separately

--- examples/assets/server.rb
+++ examples/assets/server.rb
@@ -5,12 +5,16 @@ require 'haml'
 require 'coffee_script'
 
 class App < NYNY::App
-  assets :url => '/assets', :paths => [
-    'vendor/javascripts',
-    'app/assets/javascripts',
-    'app/assets/stylesheets',
-    'app/assets/images'
-  ]
+  sprockets = Sprockets::Environment.new do |env|
+    env.append_path 'vendor/javascripts'
+    env.append_path 'app/assets/javascripts'
+    env.append_path 'app/assets/stylesheets'
+    env.append_path 'app/assets/images'
+  end
+
+  builder.map '/assets' do
+    run sprockets
+  end
 
   get '/' do
     render 'app/views/index.haml'
